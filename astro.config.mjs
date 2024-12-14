26f1b6ab2a479f3330a9c394984ffc141d23807c
Olsen Kraja <krajaolsen@gmail.com>
2024-12-14 16:36:07 +0100

    Cloudflare configs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,11 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import markdoc from '@astrojs/markdoc';
 import keystatic from '@keystatic/astro';
-
-import node from '@astrojs/node';
-
 import tailwind from '@astrojs/tailwind';
 
-// https://astro.build/config
+import cloudflare from '@astrojs/cloudflare';
+
 export default defineConfig({
   integrations: [react(), markdoc(), keystatic(), tailwind()],
-
-  adapter: node({
-    mode: 'standalone',
-  }),
+  adapter: cloudflare(),
 });
