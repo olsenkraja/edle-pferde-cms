f81fcb583d7d39d815f1880dce2aaa1ec1d00132
Olsen Kraja <krajaolsen@gmail.com>
2024-12-14 19:23:08 +0100

    Vercel configs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,9 @@ import react from '@astrojs/react';
 import markdoc from '@astrojs/markdoc';
 import keystatic from '@keystatic/astro';
 import tailwind from '@astrojs/tailwind';
-
-import cloudflare from '@astrojs/cloudflare';
+import vercel from '@astrojs/vercel';
 
 export default defineConfig({
   integrations: [react(), markdoc(), keystatic(), tailwind()],
-  adapter: cloudflare(),
+  adapter: vercel(),
 });
